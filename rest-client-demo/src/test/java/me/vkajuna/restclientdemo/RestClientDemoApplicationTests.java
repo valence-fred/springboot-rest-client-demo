c7fd4e86aae5831ac4a78301d0b2b7334450870d
Valence Fredrick Kajuna <valencekajuna@yahoo.com>
2024-04-14 17:59:02 +0300

    first commit

---
+++ rest-client-demo/src/test/java/me/vkajuna/restclientdemo/RestClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vkajuna.restclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
